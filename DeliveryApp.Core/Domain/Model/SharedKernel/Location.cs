60684e8e11bc6a57d253797f1924c7e8483b4960
am.zhuravlev <am.zhuravlev@NTB44766>
2025-02-28 09:41:35 +0300

    Added Location and LocationShould

---
+++ DeliveryApp.Core/Domain/Model/SharedKernel/Location.cs
@@ -0,0 +1,49 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using CSharpFunctionalExtensions;
+using Primitives;
+
+namespace DeliveryApp.Core.Domain.Model.SharedKernel;
+
+public class Location : ValueObject
+{
+    private const int MinCoordinate = 1;
+    private const int MaxCoordinate = 10;
+
+    public static Location Random => new Location(new Random().Next(MinCoordinate, MaxCoordinate),
+        new Random().Next(MinCoordinate, MaxCoordinate));
+    
+    [ExcludeFromCodeCoverage]
+    private Location()
+    {
+    }
+    
+    private Location(int x, int y) : this()
+    {
+        X = x;
+        Y = y;
+    }
+    
+    public int X { get; } 
+    public int Y { get; }
+
+    public static Result<Location, Error> Create(int x, int y)
+    {
+        if (x < MinCoordinate || x > MaxCoordinate)
+            return GeneralErrors.ValueIsInvalid(nameof(x));
+        if (y < MinCoordinate || y > MaxCoordinate)
+            return GeneralErrors.ValueIsInvalid(nameof(y));
+
+        return new Location(x, y);
+    }
+
+    public int CalculateDistance(Location otherLocation)
+    {
+       return Math.Abs(X - otherLocation.X) + Math.Abs(Y - otherLocation.Y);
+    }
+
+    protected override IEnumerable<IComparable> GetEqualityComponents()
+    {
+        yield return X;
+        yield return Y;
+    }
+}
---
+++ Tests/DeliveryApp.UnitTests/Domain/Model/SharedKernel/LocationShould.cs
@@ -0,0 +1,106 @@
+﻿using DeliveryApp.Core.Domain.Model.SharedKernel;
+using FluentAssertions;
+using Xunit;
+
+namespace DeliveryApp.UnitTests.Domain.Model.SharedKernel;
+
+public class LocationShould
+{
+    [Theory]
+    [InlineData(1, 1)]
+    [InlineData(10, 10)]
+    public void BeCorrectWhenParamsIsCorrectOnCreated(int x, int y)
+    {
+        // Arrange
+        
+        // Act
+        var location = Location.Create(x, y);
+        
+        // Assert
+        location.IsSuccess.Should().BeTrue();
+        location.Value.X.Should().Be(x);
+        location.Value.Y.Should().Be(y);
+    }
+    
+    [Theory]
+    [InlineData(0, 1)]
+    [InlineData(1, 0)]
+    [InlineData(-1, 1)]
+    [InlineData(1, -1)]
+    [InlineData(11, 1)]
+    [InlineData(1, 11)]
+    public void ReturnErrorWhenParamsIsInCorrectOnCreated(int x, int y)
+    {
+        // Arrange
+        
+        // Act
+        var location = Location.Create(x, y);
+        
+        // Assert
+        location.IsSuccess.Should().BeFalse();
+        location.Error.Should().NotBeNull();
+    }
+    
+    [Fact]
+    public void BeEqualWhenAllPropertiesIsEqual()
+    {
+        // Arrange
+        var first = Location.Create(5, 5).Value;
+        var second = Location.Create(5, 5).Value;
+        
+        // Act
+        var result = first == second;
+        
+        // Assert
+        result.Should().BeTrue();
+    }
+    
+    [Theory]
+    [InlineData(1, 5)]
+    [InlineData(5, 1)]
+    public void BeNotEqualWhenPartPropertiesIsNotEqual(int x, int y)
+    {
+        // Arrange
+        var first = Location.Create(5, 5).Value;
+        var second = Location.Create(x, y).Value;
+        
+        // Act
+        var result = first == second;
+        
+        // Assert
+        result.Should().BeFalse();
+    }
+    
+    [Theory]
+    [InlineData(1, 1, 8)]
+    [InlineData(10, 10, 10)]
+    [InlineData(5, 5, 0)]
+    public void BeCorrectWhenCalculateDistance(int x, int y, int resultDistance)
+    {
+        // Arrange
+        var first = Location.Create(5, 5).Value;
+        var second = Location.Create(x, y).Value;
+        
+        // Act
+        var result = first.CalculateDistance(second);
+        
+        // Assert
+        result.Should().Be(resultDistance);
+    }
+    
+    [Fact]
+    public void BeCorrectWhenRandomOnCreated()
+    {
+        // Arrange
+        
+        // Act
+        var location = Location.Random;
+        
+        // Assert
+        location.Should().NotBeNull();
+        location.X.Should().BeGreaterThan(0);
+        location.Y.Should().BeGreaterThan(0);
+        location.X.Should().BeLessThan(11);
+        location.Y.Should().BeLessThan(11);
+    }
+}
